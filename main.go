5bb5631192d88716b475c146a8fa5e4a280ff0a7
peifeng.yao@shopee.com <peifeng.yao@shopee.com>
2020-12-09 18:31:01 +0800

    v1

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/yyf-404/serialize_pb/pb"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	// 连接服务器
+	conn, err := grpc.Dial(":8972", grpc.WithInsecure())
+	if err != nil {
+		fmt.Printf("faild to connect: %v", err)
+	}
+	defer conn.Close()
+	client := serialize.NewSerializeServiceClient(conn)
+	req := serialize.MsgRequest{
+		Name:   "LiHua",
+		Id:     5,
+		IsUser: true,
+	}
+	client.SendMsg(context.Background(), &req)
+}
